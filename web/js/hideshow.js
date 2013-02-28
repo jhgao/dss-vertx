4c443e3da3c4e45d6251880e6ffb379200ff515c
theme <theme@theme-VPCEA28EC.(none)>
2013-03-01 01:19:52 +0800

    mod js in index.html

--- web/js/hideshow.js
+++ web/js/hideshow.js
@@ -3,37 +3,41 @@
 // this tells jquery to run the function below once the DOM is ready
 $(document).ready(function() {
 
-// choose text for the show/hide link - can contain HTML (e.g. an image)
-var showText='Show';
-var hideText='Hide';
+	// choose text for the show/hide link - can contain HTML (e.g. an image)
+	var showText='Show';
+	var hideText='Hide';
 
-// initialise the visibility check
-var is_visible = false;
+	// initialise the visibility check
+	var is_visible = false;
 
-// append show/hide links to the element directly preceding the element with a class of "toggle"
-$('.toggle').prev().append(' <a href="#" class="toggleLink">'+hideText+'</a>');
+	// append show/hide links to the element directly preceding the element with a class of "toggle"
+	$('.toggle').prev().append(' <a href="#" class="toggleLink">'+showText+'</a>');
 
-// hide all of the elements with a class of 'toggle'
-$('.toggle').show();
+	// hide all of the elements with a class of 'toggle'
+	$('.toggle').hide();
 
-// capture clicks on the toggle links
-$('a.toggleLink').click(function() {
+	//show first
+	$('a.toggleLink:first').text(hideText);
+	$('ul.toggle:first').show();
 
-// switch visibility
-is_visible = !is_visible;
+	// capture clicks on the toggle links
+	$('a.toggleLink').click(function() {
 
-// change the link text depending on whether the element is shown or hidden
-if ($(this).text()==showText) {
-$(this).text(hideText);
-$(this).parent().next('.toggle').slideDown('slow');
-}
-else {
-$(this).text(showText);
-$(this).parent().next('.toggle').slideUp('slow');
-}
+		// switch visibility
+		is_visible = !is_visible;
 
-// return false so any link destination is not followed
-return false;
+		// change the link text depending on whether the element is shown or hidden
+		if ($(this).text()==showText) {
+		$(this).text(hideText);
+		$(this).parent().next('.toggle').slideDown('slow');
+		}
+		else {
+		$(this).text(showText);
+		$(this).parent().next('.toggle').slideUp('slow');
+	}
 
-});
+	// return false so any link destination is not followed
+	return false;
+
+	});
 });
